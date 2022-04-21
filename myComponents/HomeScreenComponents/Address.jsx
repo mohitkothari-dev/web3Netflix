e97d3abbd07b8a768a46d1eb10fe67a84f162f68
mohitkothari <mrulekothari@gmail.com>
2022-04-21 15:38:03 +0530

    optimized useEffect errors

--- myComponents/HomeScreenComponents/Address.jsx
+++ myComponents/HomeScreenComponents/Address.jsx
@@ -1,13 +1,25 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import { useMoralis } from 'react-moralis'
 import { ShortenAddress } from '../../utils/ShortenAddress'
 
 const Address = () => {
 
+    const [userName, setUserName] = useState('0x0000000000000000000000');
+
     const { user } = useMoralis();
-    const currentUser = user.get('ethAddress');
+
+    //The user object imports after a delay
+    const getUser = () => {
+      if(!user) return null;
+      setUserName(user.get('ethAddress'))
+    }
+
+    useEffect(() => {
+      getUser();
+    });
+    
   return (
-    <div style={{ color: '#fff', marginRight: '5px' }} >{ShortenAddress(currentUser)}</div>
+    <div style={{ color: '#fff', marginRight: '5px' }} >{ShortenAddress(userName)}</div>
   )
 }
 
