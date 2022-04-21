c33166d6103da59d86000f25952c7fcd2b804ae6
mohitkothari <mrulekothari@gmail.com>
2022-04-21 13:13:43 +0530

    Optimizing SEO

--- pages/player.jsx
+++ pages/player.jsx
@@ -2,6 +2,7 @@ import React, {useEffect} from 'react'
 import { useMoralis } from 'react-moralis'
 import styles from '../styles/Player.module.css'
 import { useRouter } from 'next/router'
+import Head from 'next/head'
 
 const vid = 'netflix.mp4'
 
@@ -14,6 +15,11 @@ const Player = () => {
     }, [isAuthenticated] )
   return (
     <div className={styles.player}>
+      <Head>
+        <title>Welcome to Web3 Netflix - Stream of favorite Shows</title>
+        <meta name="description" content="A decentralized streaming service that offers wide varienty of award-winning TV shows, movies, anime, documentaries, and more." />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
         <video autoPlay controls className={styles.videoPlayer}>
             <source src={vid} type='video/mp4'/>
         </video>
